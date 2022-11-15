b5de70fa838983ffb4b2f7aa15ca7a0989a8b269
SLQPD276dW <51816533+SLQPD276dW@users.noreply.github.com>
2022-11-15 14:33:59 +0900

    delete: NavbarのTopを変更
    URLが"/"になっている時に"Top"が"Portofolio"になる機能を削除

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
       <ul className="text-gray-700 flex list-none items-center p-0 text-sm">
         <li className="border-black px-6 pb-2 hover:border-b-2">
           <a href="/" className="text-3xl">
-            {location.pathname === '/' ? 'Portofolio' : 'Top'}
+            Top
           </a>
         </li>
         <li className="border-black px-6 pb-2 hover:border-b-2">

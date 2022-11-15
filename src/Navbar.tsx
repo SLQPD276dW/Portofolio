86efb0a051e6dfeede602e9320767f89dcd5f534
SLQPD276dW <51816533+SLQPD276dW@users.noreply.github.com>
2022-11-15 14:32:10 +0900

    update: Navbarに/skillsへのリンクを追加

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -10,6 +10,11 @@ function Navbar() {
             {location.pathname === '/' ? 'Portofolio' : 'Top'}
           </a>
         </li>
+        <li className="border-black px-6 pb-2 hover:border-b-2">
+          <a href="/skills" className="text-3xl">
+            Skills
+          </a>
+        </li>
       </ul>
     </nav>
   );
